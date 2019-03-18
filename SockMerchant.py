ac37c6e00bcf79eb03da50bdd915e6e52db6f473
Syamala Nanditha Vittanala <v.shyamalananditha@gmail.com>
2019-03-18 00:25:42 -0500

    Adding Counting Valleys problem

--- SockMerchant.py
+++ SockMerchant.py
@@ -1,11 +1,17 @@
 # Problem
 #
-# John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
-# For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+# John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
+# Given an array of integers representing the color of each sock, determine how many pairs of socks
+# with matching colors there are.
+
+# For example, there are n = 7 socks with colors ar = [1, 2, 1, 2, 1, 3, 2].
+# There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color.
+# The number of pairs is 2.
 #
 # Function Description
 #
-# The sockMerchant function must return an integer representing the number of matching pairs of socks that are available.
+# The sockMerchant function must return an integer representing the number
+# of matching pairs of socks that are available.
 #
 # sockMerchant has the following parameter(s):
 #
@@ -14,8 +20,8 @@
 #
 # Input Format
 #
-# The first line contains an integer , the number of socks represented in ar.
-# The second line contains  space-separated integers describing the colors  ar[i] of the socks in the pile.
+# The first line contains an integer n, the number of socks represented in ar.
+# The second line contains n space-separated integers describing the colors ar[i] of the socks in the pile.
 #
 # Constraints
 #
@@ -30,9 +36,9 @@
 def sock_merchant(n, ar):
     """
 
-    :param n: number of
-    :param ar:
-    :return:
+    :param n: number of socks
+    :param ar: array of colors of the socks available
+    :return: number of matching color pairs of socks that can be sold
     """
     ar.sort()
     count, i = 0, 0
@@ -48,10 +54,7 @@ def sock_merchant(n, ar):
 
 
 if __name__ == '__main__':
-
     n = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sock_merchant(n, ar)
     print(result)
