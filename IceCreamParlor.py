4ebf339b4e5578ac92d4da782fc86af116780e8a
Syamala Nanditha Vittanala <v.shyamalananditha@gmail.com>
2019-05-01 18:15:59 -0500

    IceCreamParlor problem

--- IceCreamParlor.py
+++ IceCreamParlor.py
@@ -51,9 +51,6 @@ if __name__ == '__main__':
     t = int(input())
     for t_itr in range(t):
         money = int(input())
-
         n = int(input())
-
         cost = list(map(int, input().rstrip().split()))
-
         print(' '.join(map(str(what_flavors(cost, money))))
