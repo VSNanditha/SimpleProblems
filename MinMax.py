00cb17ad0aa2a31b5a7087c73fd27e507e98199c
Syamala Nanditha Vittanala <v.shyamalananditha@gmail.com>
2019-05-01 14:24:16 -0500

    Adding MinMax.py

--- MinMax.py
+++ MinMax.py
@@ -1,3 +1,29 @@
+# You will be given a list of integers, arr, and a single integer k. You must create an array of length k from
+# elements of arr such that its unfairness is minimized. Call that array subarr. Unfairness of an array is calculated as
+#                                   max(subarr) - min(subarr)
+# Where:
+# - max denotes the largest integer in subarr.
+# - min denotes the smallest integer in subarr.
+#
+# As an example, consider the array [1, 4, 7, 2] with a k of 2. Pick any two elements, test subarr = [4, 7].
+# unfairness = max(4, 7) - min(4, 7) = 7 - 4 = 3
+#
+# Testing for all pairs, the solution [1, 2] provides the minimum unfairness.
+#
+# Note: Integers in arr may not be unique.
+#
+# Input Format
+#
+# The first line contains an integer n, the number of elements in array arr.
+# The second line contains an integer k.
+# Each of the next n lines contains an integer arr[i] where 0 <= i <= n.
+#
+# Constraints
+# 2 <= n <= 10^5
+# 2 <= k <= n
+# 0 <= arr[i] <= 10^9
+
+
 def max_min(k, arr):
     """
     :param k: window size
